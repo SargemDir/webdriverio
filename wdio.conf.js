d30ef5069b108eca10b3ad2e3a9bbbb77289d9b8
SargemDir <dimonisr@yahoo.com>
2022-08-27 21:57:19 +0300

    config files added

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,6 +4,7 @@ exports.config = {
     // Runner Configuration
     // ====================
     //
+    port: 4723,
     //
     // ==================
     // Specify Test Files
@@ -20,6 +21,13 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
+    specs: [
+         //  './test/specs/**/android-native*.js'
+        // './test/specs/android/*.js'
+        // './test/specs/**/ios*.js'
+        './test/specs/walla/wallaNews*.js'
+        // './test/specs/android/delete-note-screen*.js'
+    ],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
@@ -46,6 +54,25 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
+    capabilities: [
+    
+        {
+        platformName: "Android",
+        "appium:platformVersion": "11.0",
+        "appium:deviceName": "Pixel_3",
+        "appium:automationName": "UIAutomator2",
+        "appium:app": "/Users/wallaqa/webdriverio-appium/app/android/wallaApk/com.walla-7.5.2-251-release.apk",
+        // "appium:app": "/Users/wallaqa/webdriverio-appium/app/android/ColorNote+Notepad.apk",
+        "appium:autoGrantPermissions": true
+        }
+        // {
+        //     platformName: "ios",
+        //     "appium:platformVersion": "15.5",
+        //     "appium:deviceName": "iPhone 12",
+        //     "appium:automationName": "XCUITest",
+        //     "appium:app": "/Users/wallaqa/webdriverio-appium/app/ios/UIKitCatalog.app"
+        //     }
+    ],
     //
     // ===================
     // Test Configurations
@@ -88,20 +115,20 @@ exports.config = {
     //
     // Default request retries count
     connectionRetryCount: 3,
-    // //
-    // // Test runner services
-    // // Services take over a specific job you don't want to take care of. They enhance
-    // // your test setup with almost no effort. Unlike plugins, they don't add new
-    // // commands. Instead, they hook themselves up into the test process.
-    // services: [
-    //     ['appium', {
-    //         args: {
-    //             address: 'localhost',
-    //             port: 4723
-    //         },
-    //         logPath: './'
-    //     }]
-    // ],
+    //
+    // Test runner services
+    // Services take over a specific job you don't want to take care of. They enhance
+    // your test setup with almost no effort. Unlike plugins, they don't add new
+    // commands. Instead, they hook themselves up into the test process.
+    services: [
+        ['appium', {
+            args: {
+                address: 'localhost',
+                port: 4723
+            },
+            logPath: './'
+        }]
+    ],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
